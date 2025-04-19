b84c0c29ab96bfee9428bc21ff1945447b069d00
Isaac David <orpheezt@outlook.com>
2025-04-19 12:20:33 -0500

    Hmmmm should I define the regs when I can with auto? the operation gives me the type and I can visualize it with IDE inline hints.. As well there are other cases for perf opt where I have to define most of the regs ahead of time

--- predication/predication.cpp
+++ predication/predication.cpp
@@ -13,8 +13,8 @@
 void gt_zero_add_avx512(uint64_t n, float *const __restrict x) {
   [[assume(n % 16 == 0)]];
   for (uint64_t i = 0; i < n; i += 16) {
-    __m512 xV = _mm512_loadu_ps(x + i);
-    __mmask16 mask = _mm512_cmp_ps_mask(xV, _mm512_setzero_ps(), _CMP_NLE_UQ);
+    auto xV = _mm512_loadu_ps(x + i);
+    auto mask = _mm512_cmp_ps_mask(xV, _mm512_setzero_ps(), _CMP_NLE_UQ);
     xV = _mm512_mask_add_ps(xV, mask, xV, _mm512_set1_ps(1.f));
     _mm512_storeu_ps(x + i, xV);
   }
@@ -32,8 +32,8 @@ void gt_zero_add_avx512(uint64_t n, float *const __restrict x) {
 void gt_zero_sqrt_add_lt_zero_avx512(uint64_t n, float *const __restrict x) {
   [[assume(n % 16 == 0)]];
   for (uint64_t i = 0; i < n; i += 16) {
-    __m512 xV = _mm512_loadu_ps(x + i);
-    __mmask16 mask = _mm512_cmp_ps_mask(xV, _mm512_setzero_ps(), _CMP_NLE_UQ);
+    auto xV = _mm512_loadu_ps(x + i);
+    auto mask = _mm512_cmp_ps_mask(xV, _mm512_setzero_ps(), _CMP_NLE_UQ);
     xV = _mm512_mask_sqrt_ps(xV, mask, xV);
     xV = _mm512_mask_add_ps(xV, _mm512_knot(mask), xV, _mm512_set1_ps(1.f));
     _mm512_storeu_ps(x + i, xV);
