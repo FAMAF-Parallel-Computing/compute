62c9ce46ea7a5d3a5d0cc9342d094ac5745f15fc
Isaac David <orpheezt@outlook.com>
2025-06-08 19:20:26 -0500

    QOL? still objdump on the test I see that no SIMD is applied...

--- integration/midpoint/omp_simd_test.cpp
+++ integration/midpoint/omp_simd_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <numbers>
+#include <random>
 
 #include <omp_simd.hpp>
 
@@ -12,6 +13,10 @@ using namespace std;
 float pi_fn(float x);
 
 TEST(midpointIntegrationTest, piCorrectness) {
-  const auto r = 4.f * midpoint_integration_omp(1'000, 0.f, 1.f, pi_fn);
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<uint32_t> dis(100, 1000);
+  const auto n = dis(gen);
+  const auto r = 4.f * midpoint_integration_omp(n, 0.f, 1.f, pi_fn);
   EXPECT_NEAR(r, std::numbers::pi_v<float>, 1e-5f);
 }
